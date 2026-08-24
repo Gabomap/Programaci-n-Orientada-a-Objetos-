eec49d9b786a766e548ed84075d072f78d88ba10
Gabomap <anibalmacias35@aragon.unam.mx>
2026-08-23 23:11:37 -0600

    Delete Tarea12 directory

--- Tarea12/src/MainSwing.java
+++
@@ -1,6 +0,0 @@
-public class MainSwing {
-    public static void main(String[] args) {
-        VentanaSwing v = new VentanaSwing();
-
-    }
-}
--- Tarea12/src/MainVentana2.java
+++
@@ -1,5 +0,0 @@
-public class MainVentana2 {
-    public static void main(String[] args) {
-        Ventana2 ventana = new Ventana2();
-    }
-}
